09071f13f1731d1f01262bbf0e85ba97d8b2f7a8
alapati rohith venkata sriniavs <alapatirohith91@gmail.com>
2026-09-21 21:17:13 +0530

    Use hash routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,29 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom';import Navbar from './components/Navbar';import Footer from './components/Footer';import ScrollTop from './components/ScrollTop';import Home from './pages/Home';import Menu from './pages/Menu';import About from './pages/About';import Gallery from './pages/Gallery';import Contact from './pages/Contact';import Order from './pages/Order';import {CartProvider} from './context/CartContext';export default function App(){return <BrowserRouter><CartProvider><ScrollTop/><Navbar/><Routes><Route path="/" element={<Home/>}/><Route path="/menu" element={<Menu/>}/><Route path="/about" element={<About/>}/><Route path="/gallery" element={<Gallery/>}/><Route path="/contact" element={<Contact/>}/><Route path="/order" element={<Order/>}/></Routes><Footer/></CartProvider></BrowserRouter>}
+import {HashRouter,Routes,Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import ScrollTop from './components/ScrollTop';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import About from './pages/About';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
+import Order from './pages/Order';
+import {CartProvider} from './context/CartContext';
+
+export default function App(){
+  return <HashRouter>
+    <CartProvider>
+      <ScrollTop/>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/menu" element={<Menu/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/gallery" element={<Gallery/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/order" element={<Order/>}/>
+      </Routes>
+      <Footer/>
+    </CartProvider>
+  </HashRouter>
+}
